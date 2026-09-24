53b44455e170d51252f06c44dcc5052e941711dd
Idowu Ibraheem <ibraheemidowu851@gmail.com>
2026-09-24 06:05:05 -0700

    Ready for demo

--- avix_api/settings.py
+++ avix_api/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,12 +83,17 @@ WSGI_APPLICATION = 'avix_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
+
 
 
 # Password validation
